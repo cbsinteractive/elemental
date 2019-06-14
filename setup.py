b90d11b962df791476f3edd45c0f415c533b1b79
Tianchen Min <tianchenmin@gmail.com>
2019-06-14 16:17:53 -0400

    fixes

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('requirements.txt') as f:
 setup(
     name='elemental',
     version='0.1',
-    packages=['elemental',],
-    license='MIT',
+    url='https://github.com/cbsinteractive/elemental.git',
+    license='unlicense',
     long_description=open('README.md').read(),
     install_requires=requirements,
-    package_dir={'':'elemental'}
+    packages=['elemental']
 )
