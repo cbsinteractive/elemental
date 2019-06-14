d2e68adac0afc7492dd5938f5933ed89196278ec
Tianchen Min <tianchenmin@gmail.com>
2019-06-14 16:10:08 -0400

    fixes

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open('requirements.txt') as f:
 
 setup(
     name='elemental',
-    version='0.1dev',
+    version='0.1',
     packages=['elemental',],
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=requirements,
+    package_dir={'':'elemental'}
 )
