b82c1bba7c3e25bfe89c2cd8e3de7d923780c6d1
Tianchen Min <tianchenmin@gmail.com>
2019-06-17 17:43:09 -0400

    1. Add 'device_name' and 'channel' params to template 2. Users can now use two ways to create ElementalClient, with authentication and without authentication

--- live/client.py
+++ live/client.py
@@ -1,21 +1,37 @@
 import requests
 from jinja2 import Template
+import time
+from urllib.parse import urlparse
+import hashlib
 
 
 class ElementalLive():
-    def __init__(self, server_ip):
+    def __init__(self, server_ip, user=None, api_key=None):
         self.server_ip = server_ip
+        self.user = user
+        self.api_key = api_key
 
-    # Simple version of generate_header,
-    # may need more paras in the future(like api_key)
-    def generate_headers(self):
+    def generate_headers_with_authentication_enabled(self, url, user, api_key):
+        expiration = int(time.time() + 120)
+        parse = urlparse(url)
+        prehash = "%s%s%s%s" % (parse.path, user, api_key, expiration)
+        digest = hashlib.md5(prehash.encode('utf-8')).hexdigest()
+        final_hash = "%s%s" % (api_key, digest)
+        key = hashlib.md5(final_hash.encode('utf-8')).hexdigest()
+        return {
+            'X-Auth-User': user,
+            'X-Auth-Expires': str(expiration),
+            'X-Auth-Key': key,
+            'Accept': 'application/xml',
+            'Content-Type': 'application/xml'
+        }
+
+    def generate_header_with_authentication_disabled(self):
         return {
             'Accept': 'application/xml',
             'Content-Type': 'application/xml'
         }
 
-    # Create event, options contain username,
-    # password and mediastore_container url
     def create_event(self, template_path, options):
 
         # Initiate url
@@ -29,9 +45,16 @@ class ElementalLive():
         # Pass params to template
         body = template.render(**options)
 
+        # Generate headers according to how users create ElementalLive class
+        if self.user is None and self.api_key is None:
+            headers = self.generate_header_with_authentication_disabled()
+        else:
+            headers = self.generate_headers_with_authentication_enabled(
+                url, self.user, self.api_key)
+
         # Send request
         response = requests.request(method='POST', url=url, data=body,
-                                    headers=self.generate_headers())
+                                    headers=headers)
 
         return response
 
@@ -40,8 +63,16 @@ class ElementalLive():
         # Initial url
         url = f'{self.server_ip}/live_events/{event_id}'
 
+        # Generate headers according to how users create ElementalLive class
+        if self.user is None and self.api_key is None:
+            headers = self.generate_header_with_authentication_disabled()
+        else:
+            headers = self.generate_headers_with_authentication_enabled(
+                url, self.user, self.api_key)
+
         # Send request
-        response = requests.request(method='DELETE', url=url)
+        response = requests.request(method='DELETE', url=url,
+                                    headers=headers)
 
         return response
 
@@ -53,9 +84,16 @@ class ElementalLive():
         # Generate body
         body = "<start></start>"
 
+        # Generate headers according to how users create ElementalLive class
+        if self.user is None and self.api_key is None:
+            headers = self.generate_header_with_authentication_disabled()
+        else:
+            headers = self.generate_headers_with_authentication_enabled(
+                url, self.user, self.api_key)
+
         # Send request
         response = requests.request(method='POST', url=url, data=body,
-                                    headers=self.generate_headers())
+                                    headers=headers)
 
         return response
 
@@ -67,8 +105,15 @@ class ElementalLive():
         # Generate body
         body = "<stop></stop>"
 
+        # Generate headers according to how users create ElementalLive class
+        if self.user is None and self.api_key is None:
+            headers = self.generate_header_with_authentication_disabled()
+        else:
+            headers = self.generate_headers_with_authentication_enabled(
+                url, self.user, self.api_key)
+
         # Send request
         response = requests.request(method='POST', url=url, data=body,
-                                    headers=self.generate_headers())
+                                    headers=headers)
 
         return response
