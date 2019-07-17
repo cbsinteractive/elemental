fc643e3adabf084dc5ec819cc94d77f02b2f6b17
Tianchen Min <tianchenmin@gmail.com>
2019-07-17 12:18:02 -0400

    remove name

--- elemental/__init__.py
+++ elemental/__init__.py
@@ -1,5 +1,4 @@
 from .client import (ElementalException, ElementalLive, InvalidRequest,
                      InvalidResponse)
 
-name = "elemental"
 __all__ = [ElementalException, ElementalLive, InvalidResponse, InvalidRequest]
