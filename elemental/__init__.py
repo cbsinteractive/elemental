d5f1311dfcc74a2d318e7e430562f36c905f3748
njannupreddy <njannupreddy@yahoo.com>
2021-07-27 12:53:35 -0400

    Minor fix for previous release

--- elemental/__init__.py
+++ elemental/__init__.py
@@ -1,4 +1,4 @@
-from .client import (ElementalException, ElementalLive, InvalidRequest,
+from .client import (ConnectionTimeout, ElementalException, ElementalLive, InvalidRequest,
                      InvalidResponse, NotFound)
 
-__all__ = ('ElementalException', 'ElementalLive', 'InvalidResponse', 'InvalidRequest', 'NotFound')
+__all__ = ('ConnectionTimeout', 'ElementalException', 'ElementalLive', 'InvalidResponse', 'InvalidRequest', 'NotFound')
